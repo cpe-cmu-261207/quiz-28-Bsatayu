8c8d7db7e10b6b93c471107120806ef4fbbcf66e
Bsatayu <satayu_s@cmu.ac.th>
2022-08-04 14:09:54 +0700

    commit

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+
+
+const btnRandom = document.getElementById("div-loading-card")
+const RandomPeopleImages = document.getElementById("img-profile")
+
 async function callApi() {
+
+
   const resp = await axios.get("https://randomuser.me/api/");
+  RandomPeopleImages.src = resp.data.message
 }
+
+
+
+
+RandomPeopleImages.onload = () =>{
+  
+  btnRandom.disabled = false
+  setTimeout (()=>{
+     RandomPeopleImages.className = ""
+  },1000)
+ }
